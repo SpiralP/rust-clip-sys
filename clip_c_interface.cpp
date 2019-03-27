c060e5f377e70110754d82d6a828f182f7c44ee5
SpiralP <spiral.palmer@gmail.com>
2019-03-27 15:02:19 -0700

    clip interface

--- clip_c_interface.cpp
+++ clip_c_interface.cpp
@@ -1,101 +1,46 @@
 // Clip Library
 // Copyright (c) 2015-2016 David Capello
 
-#include <iomanip>
-#include <iostream>
+#include <cstdint>
 #include "clip.h"
 
-template <typename T>
-static void print_channel(const clip::image& img,
-                          const clip::image_spec& spec,
-                          const std::string& channel_name,
-                          const int channel_mask,
-                          const int channel_shift,
-                          const int channel_fill_width,
-                          const int pixel_size) {
-  std::cout << channel_name << ":\n";
-  for (unsigned long y = 0; y < spec.height; ++y) {
-    const char* p = (img.data() + spec.bytes_per_row * y);
-    std::cout << "  ";
-    for (unsigned long x = 0; x < spec.width; ++x, p += pixel_size) {
-      std::cout << std::right << std::hex << std::setw(channel_fill_width)
-                << (((*((T*)p)) & channel_mask) >> channel_shift) << " ";
-    }
-    std::cout << "\n";
-  }
+extern "C" uint8_t clip_empty_format() {
+  return static_cast<uint8_t>(clip::empty_format());
 }
 
-template <typename T>
-static void print_channels(const clip::image& img,
-                           const clip::image_spec& spec,
-                           const int channel_fill_width,
-                           int pixel_size = sizeof(T)) {
-  print_channel<T>(img, spec, "Red", spec.red_mask, spec.red_shift,
-                   channel_fill_width, pixel_size);
-  print_channel<T>(img, spec, "Green", spec.green_mask, spec.green_shift,
-                   channel_fill_width, pixel_size);
-  print_channel<T>(img, spec, "Blue", spec.blue_mask, spec.blue_shift,
-                   channel_fill_width, pixel_size);
-  if (spec.alpha_mask)
-    print_channel<T>(img, spec, "Alpha", spec.alpha_mask, spec.alpha_shift,
-                     channel_fill_width, pixel_size);
+extern "C" uint8_t clip_text_format() {
+  return static_cast<uint8_t>(clip::text_format());
 }
 
-int mainn() {
-  if (!clip::has(clip::image_format())) {
-    std::cout << "Clipboard doesn't contain an image\n";
-    return 1;
-  }
-
-  clip::image img;
-  if (!clip::get_image(img)) {
-    std::cout << "Error getting image from clipboard\n";
-    return 1;
-  }
-
-  clip::image_spec spec = img.spec();
-
-  std::cout << "Image in clipboard " << spec.width << "x" << spec.height << " ("
-            << spec.bits_per_pixel << "bpp)\n"
-            << "Format:"
-            << "\n"
-            << std::hex << "  Red   mask: " << spec.red_mask << "\n"
-            << "  Green mask: " << spec.green_mask << "\n"
-            << "  Blue  mask: " << spec.blue_mask << "\n"
-            << "  Alpha mask: " << spec.alpha_mask << "\n"
-            << std::dec << "  Red   shift: " << spec.red_shift << "\n"
-            << "  Green shift: " << spec.green_shift << "\n"
-            << "  Blue  shift: " << spec.blue_shift << "\n"
-            << "  Alpha shift: " << spec.alpha_shift << "\n";
-
-  std::cout << "Bytes:\n";
-  for (unsigned long y = 0; y < spec.height; ++y) {
-    char* p = img.data() + spec.bytes_per_row * y;
-    std::cout << "  ";
-    for (unsigned long x = 0; x < spec.width; ++x) {
-      for (unsigned long byte = 0; byte < spec.bits_per_pixel / 8; ++byte, ++p)
-        std::cout << std::right << std::hex << std::setfill('0') << std::setw(2)
-                  << int((*p) & 0xff) << " ";
-    }
-    std::cout << "\n";
-  }
-
-  switch (spec.bits_per_pixel) {
-    case 16:
-      print_channels<uint16_t>(img, spec, 2);
-      break;
-    case 24:
-      print_channels<uint32_t>(img, spec, 2, 3);
-      break;
-    case 32:
-      print_channels<uint32_t>(img, spec, 2);
-      break;
-    case 64:
-      print_channels<uint64_t>(img, spec, 4);
-      break;
-  }
+extern "C" uint8_t clip_image_format() {
+  return static_cast<uint8_t>(clip::image_format());
 }
 
-extern "C" int test() {
-  return mainn();
+extern "C" bool clip_has(uint8_t format) {
+  return clip::has(static_cast<uint8_t>(format));
 }
+
+extern "C" void* clip_get_image() {
+  clip::image* img = new clip::image;
+  if (!clip::get_image(*img)) {
+    return nullptr;
+  }
+
+  return img;
+}
+
+extern "C" clip::image_spec clip_get_image_spec(void* ptr) {
+  clip::image* img = static_cast<clip::image*>(ptr);
+  return img->spec();
+}
+
+extern "C" char* clip_get_image_data(void* ptr) {
+  clip::image* img = static_cast<clip::image*>(ptr);
+  return img->data();
+}
+
+extern "C" void clip_delete_image(void* ptr) {
+  clip::image* img = static_cast<clip::image*>(ptr);
+  delete img;
+}
+// const char* data = img.data();
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,83 @@
-use std::os::raw::c_int;
+use std::os::raw::{c_char, c_ulong, c_void};
+
+pub type ClipFormat = u8;
+
+pub type ClipImage = *const c_void;
+pub type ClipImageData = *const c_char;
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct ClipImageSpec {
+  pub width: c_ulong,
+  pub height: c_ulong,
+  pub bits_per_pixel: c_ulong,
+  pub bytes_per_row: c_ulong,
+  pub red_mask: c_ulong,
+  pub green_mask: c_ulong,
+  pub blue_mask: c_ulong,
+  pub alpha_mask: c_ulong,
+  pub red_shift: c_ulong,
+  pub green_shift: c_ulong,
+  pub blue_shift: c_ulong,
+  pub alpha_shift: c_ulong,
+}
 
 #[link(name = "clip_c_interface")]
 extern "C" {
-  fn test() -> c_int;
+  pub fn clip_empty_format() -> ClipFormat;
+  pub fn clip_text_format() -> ClipFormat;
+  pub fn clip_image_format() -> ClipFormat;
+
+  pub fn clip_has(format: ClipFormat) -> bool;
+
+  pub fn clip_get_image() -> ClipImage;
+  pub fn clip_delete_image(img: ClipImage);
+
+  pub fn clip_get_image_spec(img: ClipImage) -> ClipImageSpec;
+  pub fn clip_get_image_data(img: ClipImage) -> ClipImageData;
 }
 
 #[test]
-fn test_test() {
+fn test_linking() {
   unsafe {
-    println!("{}", test());
+    assert_eq!(clip_empty_format(), 0);
+    assert_eq!(clip_text_format(), 1);
+    assert_eq!(clip_image_format(), 2);
+  }
+}
+
+#[test]
+fn test_has_empty() {
+  unsafe {
+    assert!(clip_has(clip_empty_format()));
+  }
+}
+
+#[test]
+fn test_has_text() {
+  unsafe {
+    assert!(clip_has(clip_text_format()));
+  }
+}
+
+#[test]
+fn test_has_image() {
+  unsafe {
+    assert!(clip_has(clip_image_format()));
+  }
+}
+
+#[test]
+fn test_get_image() {
+  unsafe {
+    assert!(clip_has(clip_image_format()));
+
+    let img = clip_get_image();
+    assert!(!img.is_null());
+
+    println!("{:#?}", clip_get_image_spec(img));
+    println!("{:#?}", clip_get_image_data(img));
+
+    clip_delete_image(img);
   }
 }

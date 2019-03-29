26cdf49e71392567f4142b2e91506f074dd7e98f
Travis CI User <travis@example.org>
2019-03-29 05:06:02 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["clip_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"clip_sys/struct.CClipImageSpec.html\" title=\"struct clip_sys::CClipImageSpec\">CClipImageSpec</a>",synthetic:false,types:["clip_sys::CClipImageSpec"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["clip_sys"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();

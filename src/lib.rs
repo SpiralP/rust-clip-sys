40a09cde0e3885c7d3266463b7df62346637d4c5
SpiralP <spiral.palmer@gmail.com>
2020-03-01 23:23:28 -0800

    move linter command to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-#![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+mod bindings;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub use crate::bindings::*;
 
 #[test]
 fn test_linking() {

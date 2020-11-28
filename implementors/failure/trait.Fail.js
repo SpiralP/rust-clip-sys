fb4631e7aefb07f43e7c478f12c1fa2195b905c1
SpiralP <SpiralP@users.noreply.github.com>
2020-11-28 03:34:38 +0000

    deploy: 17d7d755f81a65a41825aea5685da07933975134

--- implementors/failure/trait.Fail.js
+++ implementors/failure/trait.Fail.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["clip_sys"] = [{"text":"impl <a class=\"trait\" href=\"failure/trait.Fail.html\" title=\"trait failure::Fail\">Fail</a> for <a class=\"enum\" href=\"clip_sys/enum.FFIError.html\" title=\"enum clip_sys::FFIError\">FFIError</a>","synthetic":false,"types":["clip_sys::bindings::FFIError"]}];
+implementors["clip_sys"] = [{"text":"impl Fail for FFIError","synthetic":false,"types":[]}];
 implementors["failure"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
